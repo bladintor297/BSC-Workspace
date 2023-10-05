e5d1d0ce90e1220a8a9746e768793a24aa12f8c4
NabilAsyraf2904 <nabilasyraf215@gmail.com>
2023-10-05 10:30:54 +0800

    role
    passing role id to navbar

--- src/main/java/com/bsc/controller/Login.java
+++ src/main/java/com/bsc/controller/Login.java
@@ -18,71 +18,79 @@ import javax.servlet.http.HttpSession;
  */
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		HttpSession session = request.getSession(); 
-		
-		if (session.getAttribute("email") == null)
-		response.sendRedirect("login.jsp");
-		
-		else 
-			response.sendRedirect("/bsc/Welcome");
-		response.getWriter().append("This is a get function in Login.java \nHantar user balik pegi login page. \nRedirect sini when no session created");
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+
+		if (session.getAttribute("email") == null)
+			response.sendRedirect("login.jsp");
+
+		else
+			response.sendRedirect("/bsc/Welcome");
+		response.getWriter().append(
+				"This is a get function in Login.java \nHantar user balik pegi login page. \nRedirect sini when no session created");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		// TODO Auto-generated method stub
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		HttpSession session = request.getSession();
 		RequestDispatcher dispatcher = null;
-		
+
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bsc?allowPublicKeyRetrieval=true&useSSL=false", "root", "@dmin123");
-			PreparedStatement pst = con.prepareStatement("select * from users where email='" + email + "' and password='" + password + "'");
-			
+			Connection con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/bsc?allowPublicKeyRetrieval=true&useSSL=false", "root", "@dmin123");
+			PreparedStatement pst = con.prepareStatement(
+					"select * from users where email='" + email + "' and password='" + password + "'");
+
 			ResultSet rs = pst.executeQuery();
 
-			if(rs.next()) {
+			if (rs.next()) {
 				System.out.println("Before session");
-				
+
 				// Create Session
 				session.setAttribute("id", rs.getString("id"));
 				session.setAttribute("email", rs.getString("email"));
 				session.setAttribute("name", rs.getString("name"));
 				session.setAttribute("phone", rs.getString("phone"));
+				session.setAttribute("role", rs.getInt("role"));
 				System.out.println("After session");
+
 				
-				/*
-				 * session.getAttribute("email"); session.getAttribute("nama");
-				 * System.out.println("After session");
-				 * 
-				 */ response.sendRedirect("/bsc/Welcome");
-			}else {
+				  session.getAttribute("email"); session.getAttribute("nama"); session.getAttribute("role");
+				  System.out.println("After session");
+				  
+				 response.sendRedirect("/bsc/Welcome");
+			} else {
 				session.setAttribute("status", "failed");
 				System.out.println("Wrong email or password");
 
 				dispatcher = request.getRequestDispatcher("login.jsp");
 			}
 			dispatcher.forward(request, response);
-		} catch(Exception ex){
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
--- src/main/webapp/inc/navbar.jsp
+++ src/main/webapp/inc/navbar.jsp
@@ -2,9 +2,10 @@
 <!-- Remove "navbar-sticky" class to make navigation bar scrollable with the page -->
 <header class="header navbar navbar-expand-lg bg-dark navbar-sticky shadow shadow-sm shadow-white">
 	<div class="container ps-3">
-		<a href="welcome.jsp" class="navbar-brand pe-1"> <img src="assets/img/logonew.png" width="100" alt="Black Screen Cinema"></a>
+		<a href="/bsc/Welcome" class="navbar-brand pe-1"> <img src="assets/img/logonew.png" width="100" alt="Black Screen Cinema"></a>
 		<div id="navbarNav" class="offcanvas offcanvas-end">
-		<% if (session != null && session.getAttribute("email") != null) { %> 
+		<% if (session != null && session.getAttribute("email") != null) {
+			if ((int)session.getAttribute("role") == 1 || (int)session.getAttribute("role") == 2){%> 
 			<div class="offcanvas-header border-bottom">
 				<h5 class="offcanvas-title">Menu</h5>
 				<button type="button" class="btn-close" data-bs-dismiss="offcanvas"
@@ -34,7 +35,32 @@
 		<a href="/bsc/Logout" class="btn btn-link text-warning btn-sm fs-sm rounded d-none d-lg-inline-flex ms-1 rounded-pill"> 
 			<i class='bx bx-log-out  fs-5 lh-1 me-1 text-warning'></i>
 		</a>
-		<% } else {%>
+		<%} else{ %>
+		<div class="offcanvas-header border-bottom">
+				<h5 class="offcanvas-title">Menu</h5>
+				<button type="button" class="btn-close" data-bs-dismiss="offcanvas"
+					aria-label="Close"></button>
+			</div>
+			<div class="offcanvas-body">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item"><a href="welcome.jsp" class="nav-link active">Home</a></li>
+					<li class="nav-item"><a href="docs/getting-started.html" class="nav-link">History</a></li>
+				</ul>
+			</div>
+			<div class="offcanvas-header border-top">
+
+				<a href="../profile" class="btn btn-warning w-100" target="_blank" rel="noopener"> 
+					<i class="bx bx-cart fs-4 lh-1 me-1"></i> &nbsp;My Profile
+				</a>
+			</div>
+		</div>
+		<a href="/bsc/Profile" class="btn btn-warning btn-sm fs-sm rounded d-none d-lg-inline-flex" target="_self" rel="noopener"> 
+			<i class='bx bxs-user-circle fs-5 lh-1 me-1'></i> &nbsp;My Profile
+		</a>
+		<a href="/bsc/Logout" class="btn btn-link text-warning btn-sm fs-sm rounded d-none d-lg-inline-flex ms-1 rounded-pill"> 
+			<i class='bx bx-log-out  fs-5 lh-1 me-1 text-warning'></i>
+		</a>
+		<% }} else {%>
 			<div class="d-flex justify-content-end">
 <a
 				href="/bsc/Login"
