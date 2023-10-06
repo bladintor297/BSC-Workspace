efedf7e4c9c446272936632704d75eac8e031dca
bladintor297 <85669363+bladintor297@users.noreply.github.com>
2023-10-06 08:50:43 +0800

    booking-history

--- src/main/java/com/bsc/beans/Bookings.java
+++ src/main/java/com/bsc/beans/Bookings.java
@@ -5,14 +5,54 @@ public class Bookings {
 	private int BookingID;
 	private int UserID;
 	private int MovieSlotID;
-	private int PaymentID;
+	private String PaymentID;
 	private String Seat;
 	private double Amount;
 	private String BookingDate;
 	private int Status;
+	private Movies movie;
+	private int MovieID;
+	private String Title;
+	private String Description;
+	private String ReleaseDate;
+	private int Classification;
+	private String Genre;
+	private String ImageLandscape;
+	private String ImagePortrait;
+	private String slot;
+	private String mall;
+	private String hall; 
+    
 	
+	public Bookings(int bookingID, int userID, int movieSlotID, String paymentID, String seat, double amount,
+			String bookingDate, int status, int movieID, String title, String description, String releaseDate, 
+			int classification, String genre, String imageLandscape, String imagePortrait, String Slot, String Mall, String Hall) {
+		super();
+		BookingID = bookingID;
+		UserID = userID;
+		MovieSlotID = movieSlotID;
+		PaymentID = paymentID;
+		Seat = seat;
+		Amount = amount;
+		BookingDate = bookingDate;
+		Status = status;
+		MovieID = movieID;
+		Title = title;
+		Description = description;
+		ReleaseDate = releaseDate;
+		Classification = classification;
+		Genre = genre;
+		ImageLandscape = imageLandscape;
+		ImagePortrait = imagePortrait;
+		slot = Slot;
+		mall = Mall;
+		hall = Hall;
+	}
+
+
+
 	// For Select From
-	public Bookings(int bookingID, int userID, int movieSlotID, int paymentID, 
+	public Bookings(int bookingID, int userID, int movieSlotID, String paymentID, 
 			String seat, double amount, String bookingDate, int status) {
 		BookingID = bookingID;
 		UserID = userID;
@@ -25,7 +65,7 @@ public class Bookings {
 	}
 	
 	// For Insert Into
-	public Bookings(int userID, int movieSlotID, int paymentID, 
+	public Bookings(int userID, int movieSlotID, String paymentID, 
 			String seat, double amount, String bookingDate, int status) {
 		UserID = userID;
 		MovieSlotID = movieSlotID;
@@ -35,6 +75,10 @@ public class Bookings {
 		BookingDate = bookingDate;
 		Status = status;
 	}
+	
+	public Movies getMovie() {
+        return movie;
+    }
 	
 	public int getBookingID() {
 		return BookingID;
@@ -54,10 +98,10 @@ public class Bookings {
 	public void setMovieSlotID(int movieSlotID) {
 		MovieSlotID = movieSlotID;
 	}
-	public int getPaymentID() {
+	public String getPaymentID() {
 		return PaymentID;
 	}
-	public void setPaymentID(int paymentID) {
+	public void setPaymentID(String paymentID) {
 		PaymentID = paymentID;
 	}
 	public String getSeat() {
@@ -83,6 +127,91 @@ public class Bookings {
 	}
 	public void setStatus(int status) {
 		Status = status;
+	}
+	
+	public int getMovieID() {
+		return MovieID;
+	}
+	public void setMovieID(int movieID) {
+		MovieID = movieID;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	public String getReleaseDate() {
+		return ReleaseDate;
+	}
+	public void setReleaseDate(String releaseDate) {
+		ReleaseDate = releaseDate;
+	}
+	public int getClassification() {
+		return Classification;
+	}
+	public void setClassification(int classification) {
+		Classification = classification;
+	}
+	public String getGenre() {
+		return Genre;
+	}
+	public void setGenre(String genre) {
+		Genre = genre;
+	}
+	public String getImageLandscape() {
+		return ImageLandscape;
+	}
+	public void setImageLandscape(String imageLandscape) {
+		ImageLandscape = imageLandscape;
+	}
+	public String getImagePortrait() {
+		return ImagePortrait;
+	}
+	public void setImagePortrait(String imagePortrait) {
+		ImagePortrait = imagePortrait;
+	}
+
+
+
+	public String getSlot() {
+		return slot;
+	}
+
+
+
+	public void setSlot(String slot) {
+		this.slot = slot;
+	}
+
+
+
+	public String getMall() {
+		return mall;
+	}
+
+
+
+	public void setMall(String mall) {
+		this.mall = mall;
+	}
+
+
+
+	public String getHall() {
+		return hall;
+	}
+
+
+
+	public void setHall(String hall) {
+		this.hall = hall;
 	}
 	
 	
--- src/main/java/com/bsc/controller/Booking.java
+++ src/main/java/com/bsc/controller/Booking.java
@@ -1,10 +1,24 @@
 package com.bsc.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.bsc.beans.Bookings;
+import com.stripe.model.billingportal.Session;
+
+
 
 /**
  * Servlet implementation class Booking
@@ -24,8 +38,111 @@ public class Booking extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.sendRedirect("/bsc/booking-history.jsp");
+		
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out = response.getWriter()) {
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Servlet StudentServlet</title>");
+			out.println("</head>");
+
+			ArrayList<Bookings> bookinglist = new ArrayList<>();
+			HttpSession session = request.getSession();
+			
+			try {
+
+				// Connect DB
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection con = DriverManager.getConnection(
+						"jdbc:mysql://localhost:3306/bsc?allowPublicKeyRetrieval=true&useSSL=false", "root",
+						"@dmin123");
+				
+				String query = "SELECT DISTINCT booking.*, movie.* "
+						+ "FROM booking "
+						+ "INNER JOIN movie ON booking.MovieID = movie.MovieID "
+						+ "WHERE booking.UserID = ? "
+						+ "AND booking.BookingDate >= NOW()";
+				
+				PreparedStatement preparedStatement = con.prepareStatement(query);
+				preparedStatement.setInt(1, (int)session.getAttribute("id")); 
+				ResultSet resultSet = preparedStatement.executeQuery();
+				ResultSet resultSet2 = null;
+				String _slot = null;
+				String _mall = null;
+				String _hall = null;
+				
+				while (resultSet.next()) {
+					int movieID = resultSet.getInt("MovieID");
+					
+					query = "SELECT movieslot.*, "
+							 + "mall.MallName AS MallName, "
+							 + "hall.HallName AS HallName "
+				             + "FROM movieslot "
+				             + "INNER JOIN mall ON movieslot.Mall = mall.MallID "
+				             + "INNER JOIN hall ON movieslot.Hall = hall.HallID "
+				             + "WHERE movieslot.MovieID = '" + movieID + "' "
+				             + "LIMIT 1";
+
+					preparedStatement = con.prepareStatement(query);
+					resultSet2 = preparedStatement.executeQuery();
+					while (resultSet2.next()) {
+						_slot = resultSet2.getString("Slot");
+						_mall = resultSet2.getString("MallName");
+						_hall = resultSet2.getString("HallName");
+					}
+					
+				    int bookingID = resultSet.getInt("BookingID");
+				    int userID = resultSet.getInt("UserID");
+				    int movieSlotID = resultSet.getInt("MovieSlotID");
+				    String slot =  _slot;
+				    String mall =  _mall;
+				    String hall =  _hall;
+				    String paymentID = resultSet.getString("PaymentID");
+				    String seat = resultSet.getString("Seat");
+				    double amount = resultSet.getDouble("Amount");
+				    String bookingDate = resultSet.getString("BookingDate");
+				    int status = resultSet.getInt("Status");
+				    
+				    
+				    String title = resultSet.getString("Title");
+				    String description = resultSet.getString("Description");
+				    String releaseDate = resultSet.getString("ReleaseDate");
+				    int classification = resultSet.getInt("Classification");
+				    String genre = resultSet.getString("Genre");
+				    String imageLandscape = resultSet.getString("ImageLandscape");
+				    String imagePortrait = resultSet.getString("ImagePortrait");
+				    
+				    // Create a Bookings object that combines data from both Bookings and Movies
+				    Bookings booking = new Bookings(bookingID, userID, movieSlotID, paymentID,
+				            seat, amount, bookingDate, status,
+				            movieID, title, description, releaseDate,
+				            classification, genre, imageLandscape, imagePortrait, slot, mall, hall);
+
+				    // Add the Booking object to the ArrayList
+				    bookinglist.add(booking);
+				}
+				
+				
+				// Close resources
+				con.close();
+				// resultSet2.close();
+				// preparedStatement2.close();
+				
+			} catch (Exception e) { 
+				e.printStackTrace();
+			}
+			
+			request.setAttribute("bookinglist", bookinglist);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("booking-history.jsp");
+			rd.forward(request, response);
+			
+			out.println("</body>");
+			out.println("</html>");
+		}
+		
+		
 	}
 
 	/**
